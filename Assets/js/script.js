62ef811650a838edb1dc7a6ef3263cd1f5ef7002
strongjaw15 <119770691+strongjaw15@users.noreply.github.com>
2023-01-03 17:15:55 -0600

    Added buttons, style, and began the JS.

---
+++ Assets/js/script.js
@@ -0,0 +1,17 @@
+
+// This is the beginning function, called by clicking the start button.
+function start(){
+  
+}
+
+// This starts the timer.
+function startTimer(){
+  
+}
+
+// This poses the questions.
+function askQuestion(){
+
+}
+
+document.getElementById("startButton").addEventListener("click", start())
