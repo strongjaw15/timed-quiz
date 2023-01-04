d931792068872388c2f2b043be6e6c8c8a22c028
strongjaw15 <119770691+strongjaw15@users.noreply.github.com>
2023-01-03 21:28:17 -0600

    Added a few more style elements, worked on the JavaScript, mainly in the areas of the event listener for the start button and the timer function.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,17 +1,40 @@
+let secondsLeft = 90;
+let countdown;
+let secondsDisplay = document.getElementsByTagName("h2");
+let startButton = document.getElementById("startButton")
 
-// This is the beginning function, called by clicking the start button.
+// This is the start function, called by clicking the start button.
 function start(){
-  
+  // askQuestion();
+  startTimer;
+  console.log("hi");
 }
 
 // This starts the timer.
 function startTimer(){
-  
+  countdown = setInterval(timer, 1000);
 }
 
-// This poses the questions.
+// This is the timer.
+function timer(){
+  secondsLeft--;
+  secondsDisplay.textContent="seconds left";
+
+  if(secondsLeft === 0){
+    clearInterval(countdown);
+    // sendMessage();
+  }
+}
+
+// This poses each question.
 function askQuestion(){
 
 }
 
-document.getElementById("startButton").addEventListener("click", start())
+// This sends the message at the end of the quiz.
+function sendMessage(){
+
+}
+
+// This is the event listener for the start button. It initiates the quiz.
+startButton.addEventListener("click", start);
