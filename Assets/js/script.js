0bf8b0bbecde49a12d1806f3264a314fc92e6893
strongjaw15 <119770691+strongjaw15@users.noreply.github.com>
2023-01-04 22:23:21 -0600

    Worked on the askQuestion function. Still needs lot of work to properly populate the HTML and cycle the question bank.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,40 +1,73 @@
-let secondsLeft = 91;
+let secondsLeft = 90;
 let countdown;
-let secondsDisplay = document.getElementById("timer");
-let startButton = document.getElementById("startButton");
+let question;
+let questionContent;
+let questionAnswersList;
+let questionAnswers;
+
+const questionBox = document.getElementById("questionBox");
+const secondsDisplay = document.getElementById("timer");
+const startButton = document.getElementById("startButton");
+
+let questions = [
+  {q1: "How many toes does a fish have?",
+    a: 2,
+    b: 3,
+    c: 10,
+    d: 27},
+  {q2: "How many wings on a cow?",
+    a: 1,
+    b: 4,
+    c: 5,
+    d: 12}
+]
 
 // This is the start function, called by clicking the start button.
 function start(){
-  // askQuestion();
+  questionBox.innerHTML="";
+  askQuestion();
   startTimer();
 }
 
 // This starts the timer.
 function startTimer(){
-  countdown = setInterval(timer(), 1000);
+  countdown = setInterval(timer, 200);
 }
 
 // This is the timer.
 function timer(){
-  secondsLeft--;
   secondsDisplay.textContent=`${secondsLeft} Seconds Left`;
+  secondsLeft--;
 
-  if(secondsLeft === 0){
+  if(secondsLeft < 0){
     clearInterval(countdown);
-    // sendMessage();
-    console.log("timer started");
+    secondsDisplay.textContent="";
+    secondsLeft = 90;
+    sendMessage();
   }
 }
 
-// This poses each question.
+// This poses the questions. Needs lots of work.
 function askQuestion(){
+  question = document.createElement("h3");
+  // let questionContent = document.createTextNode(`question`);
+  // question.appendChild(questionContent);
+  question.textContent = `question`;
+  questionBox.appendChild(question);
 
+  questionAnswersList = document.createElement("ol");
+  questionBox.appendChild(questionAnswersList);
+
+  questionAnswers = document.createElement("li");
+  questionAnswers.textContent = `asd`;
+  questionAnswersList.appendChild(questionAnswers);
 }
 
 // This sends the message at the end of the quiz.
 function sendMessage(){
+  questionBox.textContent="Congrats, your score is 0!";
 
 }
 
 // This is the event listener for the start button. It initiates the quiz.
-startButton.addEventListener("click", start());
+startButton.addEventListener("click", start);
