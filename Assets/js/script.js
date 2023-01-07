1576ee5e97de80bc74cff33f0d28cc96709d09e6
strongjaw15 <119770691+strongjaw15@users.noreply.github.com>
2023-01-06 20:21:12 -0600

    Changed up the event listener setup for the buttons. finished the quiz and score tally elements. Got the input field for the initials working, and have the save button working to the point that the MOST RECENT initials and score are saved to local storage.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,12 +1,14 @@
 // To Do:
-  // determine if a right answer has been selected
-  // tally the score
-  // stop the timer if all questions are asnwered
-  // create a form to input user initials
+  // fix my buttons!!!!
   // save the score and itials
+    // in my save function I need to:
+      // get the score
+      // get the questions number
+      // get the initials from the input.
+      // save them to local storage
   // create second page that displays highscores
 
-let secondsLeft = 90;
+let secondsLeft = 60;
 let countdown;
 let questionText;
 let questionContent;
@@ -17,10 +19,24 @@ let chosenAnswer;
 let properAnswer;
 let correctAnswer;
 let scoreValue = 0;
+let scorePercentage;
+let initials;
+let initialsP;
+let initialsText;
+let submissionButton;
+let button;
+let savedScores = [];
 
+const main = document.querySelector("main");
 const questionBox = document.getElementById("questionBox");
 const secondsDisplay = document.getElementById("timer");
 const startButton = document.getElementById("startButton");
+const scoresButton = document.getElementById("scoresButton")
+const formBox = document.getElementById("initialsBox");
+
+startButton.setAttribute("class", "start");
+scoresButton.setAttribute("class", "scores");
+
 
 // questions[0].answers[i] <-- this is an example of how to navigate through an array of objects
 
@@ -40,12 +56,15 @@ function start(){
   countdown = setInterval(timer, 1000);
 
   // This sets the scoreValue at the beginning of the quiz.
-  scoreValue = 0
+  scoreValue = 0;
+
+  // This clears out the end of quiz stuff.
+  questionBox.innerHTML=""
 }
 
 // This is the timer. It also has the ask question function nesting in it so that the first question shows up at the same time as the timer.
 function timer(){
-  if(secondsLeft === 90){
+  if(secondsLeft === 60){
     askQuestion();
     hideStartButton();
   }
@@ -53,10 +72,11 @@ function timer(){
   secondsDisplay.textContent=`${secondsLeft} Seconds Left`;
   secondsLeft--;
 
-  if(secondsLeft < 0){
+  // This clears the timer and goes to the dialogue at the end of the quiz.
+  if(secondsLeft < 0 || questionNumber === questions.length){
     clearInterval(countdown);
     secondsDisplay.textContent="";
-    secondsLeft = 90;
+    secondsLeft = 60;
     sendMessage();
   }
 }
@@ -87,6 +107,7 @@ function askQuestion(){
       for(var i = 0; i < 4; i++){
         answerButton = document.createElement("button");
         answerButton.setAttribute("style", "width: 12em");
+        answerButton.setAttribute("class", "answer");
         answerButton.textContent = `${questions[questionNumber].answers[i]}`;
         answersList.appendChild(answerButton);
 
@@ -99,41 +120,96 @@ function askQuestion(){
 function advanceQuestion(){
   questionBox.innerHTML = "";
 
-  if(questionNumber < questions.length - 1){
-    questionNumber++;
+  if(questionNumber < questions.length){
     askQuestion()}
-  else{
-    sendMessage()
-  }
 }
 
 function tallyScore(){
-
-  console.log("chosen answer: " + chosenAnswer)
-  console.log("proper answer: " + properAnswer)
-
-  if(chosenAnswer === properAnswer){ // <-- This isn't working yet.
+  if(chosenAnswer === properAnswer){
     scoreValue++
   }  
 
-  console.log("score value " + scoreValue)
+  // This penalizes a wrong answer by taking time off the clock.
+  else {
+    secondsLeft = secondsLeft-5
+  }
 }
 
 // This sends the message at the end of the quiz.
 function sendMessage(){
-  questionBox.textContent=`Congrats, your score is ${scoreValue}/${questions.length}!`;
 
+  // This sets the score value to a percentage and displays it.
+  x = scoreValue;
+  y = questions.length;
+  z = x/y;
+  scorePercentage = z*100;
+  questionBox.textContent=`Congrats, your score is ${scorePercentage}%!`;
+
+  // This creates the submission text.
+  initialsP = document.createElement("p");
+  initialsP.textContent = "Please enter your initials and save your score.";
+  formBox.appendChild(initialsP);
+
+  // This creates the input field for saving the score.
+  initials = document.createElement("input");
+  initials.setAttribute("type", "text");
+  initials.setAttribute("placeholder", "INITIALS");
+  initials.setAttribute("id", "getMeForSave")
+  initials.setAttribute("style", "text-align: center");
+  formBox.appendChild(initials);
+
+  // This creates the submission button.
+  submissionButton = document.createElement("button");
+  submissionButton.setAttribute("class", "save");
+  submissionButton.textContent = `SAVE`;
+  formBox.appendChild(submissionButton);
 }
 
-// This is the event listener for the start button. It initiates the quiz.
-startButton.addEventListener("click", start);
+// This saves the score and initials to local storage.
+function saveScore(){
 
-// This is the event listener for the answer buttons.
-questionBox.addEventListener("click", function(event){
-  if(event.target.matches("button")){
+  // This fetches the saved scores and objectifies them.
+  // savedScores = localStorage.getItem("scores");
+  // JSON.parse(savedScores);
+
+  // This puts the latest initials and score into an object.
+  let newObject = {
+    initials: `${initialsText}:`,
+    score: `${scorePercentage}%`
+  }
+  savedScores.push(newObject);
+
+  // This stringifies the saves scores and sends them to local storage.
+  let stringed = JSON.stringify(savedScores);
+  localStorage.setItem("scores", stringed)
+}
+
+// This is the event listener for the buttons.
+main.addEventListener("click", function(event){
+  
+  // This is for the start button.
+  if(event.target.matches("button.start")){
+    start()
+  }
+  
+  // This is for the answer buttons.
+  else if(event.target.matches("button.answer")){
     chosenAnswer = event.target.firstChild.data;
+    questionNumber++;
     tallyScore();
     advanceQuestion();
-    console.log("button clicked " + event.target.firstChild.data)
   }
-})
+  
+  // This is for the save button.
+  else if(event.target.matches("button.save")){
+    event.preventDefault();
+    initialsText = document.getElementById("getMeForSave").value;
+    saveScore();
+  }
+});
+
+// This is the event listener for the start button. It initiates the quiz.
+// startButton.addEventListener("click", start);
+
+// This is the event listener for the save button.
+// formBox.addEventListener("click", save);
